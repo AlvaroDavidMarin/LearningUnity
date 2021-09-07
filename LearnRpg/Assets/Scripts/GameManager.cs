49a41d33d708f2db7db3c513c37b074f99f5bcce
Alvaro Marin <alvaro.m1818@gmail.com>
2021-09-07 16:30:34 -0400

    Save/Load Data

--- LearnRpg/Assets/Scripts/GameManager.cs
+++ LearnRpg/Assets/Scripts/GameManager.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+    private void Awake()
     {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //Resources
+    public List<Sprite> PlayerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    //References
+    public Player player;
+    //public Weapon weapon
+
+    //Logic
+    public int Coins;
+    public int experience;
+
+    //Save State
+    /*
+     * INT preferedSkin
+     * Int Coins
+     * Int Experience
+     * int Weaponlevel
+     */
+
+
+    public void SaveState()
     {
-        
+        string s = "";
+
+        s += "0" + "|";
+        s += Coins.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+
+        PlayerPrefs.SetString("SaveState", s);
+
+    }
+
+    public void LoadState(Scene S, LoadSceneMode mode)
+    {
+        if (!PlayerPrefs.HasKey("SaveState"))
+            return;
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
+        //Change Player Skin
+        Coins = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        //Change Weapon Level
+
+        Debug.Log("Load State");
     }
 }

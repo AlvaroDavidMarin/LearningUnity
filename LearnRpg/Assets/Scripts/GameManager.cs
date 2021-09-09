393c6b423de90cdd2c1e7e5c4e8022d691820905
Alvaro Marin <alvaro.m1818@gmail.com>
2021-09-08 22:56:40 -0400

    Floating Text

--- LearnRpg/Assets/Scripts/GameManager.cs
+++ LearnRpg/Assets/Scripts/GameManager.cs
@@ -29,10 +29,17 @@ public class GameManager : MonoBehaviour
     //References
     public Player player;
     //public Weapon weapon
-
+    public FloatingTextManager floatingTextManager;
     //Logic
     public int Coins;
     public int experience;
+
+    //floating text
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize,color ,position, motion, duration);
+    }
+
 
     //Save State
     /*
